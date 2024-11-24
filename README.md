86df4a7da053b152c7a9539186167f65fa5ed7c5
maximka608 <maxbigus24680.@gmail.com>
2024-11-24 21:43:00 +0200

    Add docker

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# papersRag
+# PaperRag
+
+docker build -t paperrag-app .
+
+docker run -p 7860:7860 paperrag-app
+
+http://localhost:7860
+
--- app/main.py
+++ app/main.py
@@ -99,4 +99,4 @@ if __name__ == '__main__':
         title="PaperRAG",
         description="RAG system for scientific papers with selectable search types"
     )
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
